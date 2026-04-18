3a570956746ac62e12caef972f39e4774bda3ccf
Carlos Capote <carlos.capote@hawara.es>
2026-04-18 15:38:10 +0100

    documentation review

--- apps/docs/.astro/content-assets.mjs
+++ apps/docs/.astro/content-assets.mjs
@@ -1,1 +1,6 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_Z1io28L from "./resources/charts/architecture.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Farchitecture.md";
+import __ASTRO_IMAGE_IMPORT_ZqwXOz from "../resources/images/first-grammar.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fquickstart%2Fgrammar.md";
+import __ASTRO_IMAGE_IMPORT_1ksxEb from "../resources/images/hello-world.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fquickstart%2Fhello-world.md";
+export default new Map([["./resources/charts/architecture.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Farchitecture.md", __ASTRO_IMAGE_IMPORT_Z1io28L], ["../resources/images/first-grammar.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fquickstart%2Fgrammar.md", __ASTRO_IMAGE_IMPORT_ZqwXOz], ["../resources/images/hello-world.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fquickstart%2Fhello-world.md", __ASTRO_IMAGE_IMPORT_1ksxEb]]);
+		
